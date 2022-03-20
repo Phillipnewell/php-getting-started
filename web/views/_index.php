4e97c2d6364d94d25609e695233f4ed575aae33d
Phill <70281330+Phillipnewell@users.noreply.github.com>
2022-03-19 22:28:55 -0600

    starting up

--- web/views/_index.php
+++ web/views/_index.php
@@ -1,3 +1,9 @@
-<section>
-    Some page content
-</section>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dusty Jars | Home</title>
+  </head>
+  <body>
+    <a href="company/about.html">About Me</a>
+  </body>
+</html>
